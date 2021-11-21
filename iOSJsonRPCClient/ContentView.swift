07e4fa48fcf8025171c9b61b77d529a3310e61e5
crgold <crgold@asu.edu>
2021-11-20 23:02:37 -0800

    update

--- iOSJsonRPCClient/ContentView.swift
+++ iOSJsonRPCClient/ContentView.swift
@@ -7,6 +7,19 @@
 
 import SwiftUI
 
+struct Location: Hashable, Codable {
+    let addressTitle: String
+    let addressStreet: String
+    let elevation: Float
+    let image: String
+    let latitude: Float
+    let longitude: Float
+    let name: String
+    let description: String
+    let category: String
+    let id: Int
+}
+
 struct LocationDetail: View {
     var text: String
     
@@ -16,21 +29,32 @@ struct LocationDetail: View {
     }
 }
 
+class ViewModel: ObservableObject {
+    
+    func fetch() {
+        guard let url = URL(string: "http://127.0.0.1:8080") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in guard let data = data, error == nil else {
+            return
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List {
-                NavigationLink("ASU",destination: LocationDetail(text: "ASU"))
-                .navigationBarTitle("Locations")
-                .toolbar {
-                    ToolbarItem(placement: .bottomBar) {
-                            Button(action: {
-                                
-                            }, label: {
-                                Text("Soem Button")
-                                })
-                    }
+        VStack {
+            NavigationView {
+                List {
+                    NavigationLink("ASU",destination: LocationDetail(text: "ASU"))
+                    .navigationBarTitle("Locations")
                 }
+            }
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Add Location")
             }
         }
     }
