1987792841803783f1775e8ab5307a3c11f61c7f
crgold <crgold@asu.edu>
2021-11-20 20:47:07 -0800

    Update ContentView.swift

--- iOSJsonRPCClient/ContentView.swift
+++ iOSJsonRPCClient/ContentView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            NavigationView {
+                List {
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*/ /*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        /*@START_MENU_TOKEN@*/ /*@PLACEHOLDER=Label Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+                    }
+                    .navigationBarTitle("Locations")
+                }
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Add Location")
+            }
+        }
     }
 }
 
