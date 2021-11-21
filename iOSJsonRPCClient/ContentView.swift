5fa056e58455a13f6d2718a69bf1e89d0bf7407c
crgold <crgold@asu.edu>
2021-11-21 06:34:22 -0800

    stuck

--- iOSJsonRPCClient/ContentView.swift
+++ iOSJsonRPCClient/ContentView.swift
@@ -15,8 +15,25 @@ struct Locations: Codable {
     }
 }
 
+struct Details: Codable {
+    var elevation: Float
+    var image: String
+    var latitude: Float
+    var name: String
+    var addressTitle: String
+    var decription: String
+    var addressStreet: String
+    var category: String
+    var longitude: Float
+}
+
+struct LocationDetails: Codable {
+    var result: Details
+}
+
 class ViewModel: ObservableObject {
     @Published var locations: Locations?
+    var locationDetails : LocationDetails?
     
     func delete(index: Int) {
         guard let url = URL(string: "http://127.0.0.1:8080") else {
@@ -78,7 +95,7 @@ class ViewModel: ObservableObject {
         task.resume()
     }
     
-    func getdetails() {
+    func getDetails(location: String) {
         guard let url = URL(string: "http://127.0.0.1:8080") else {
             return
         }
@@ -86,8 +103,8 @@ class ViewModel: ObservableObject {
         var request = URLRequest(url: url)
         let json: [String: Any] = [
             "jsonrpc": "2.0",
-            "method" : "getName",
-            "params" : [],
+            "method" : "get",
+            "params" : [location],
             "id": 3
         ]
         
@@ -95,15 +112,14 @@ class ViewModel: ObservableObject {
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("application/json", forHTTPHeaderField: "Accept")
         request.httpBody = try? JSONSerialization.data(withJSONObject: json)
-        let task = URLSession.shared.dataTask(with: request as URLRequest) { [weak self] data, _, error in guard let data = data, error == nil else {
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { data, _, error in guard let data = data, error == nil else {
             return
             }
             
             do {
-                let locations = try JSONDecoder().decode(Locations.self, from: data)
+                let locationDetails = try JSONDecoder().decode(LocationDetails.self, from: data)
                 DispatchQueue.main.async {
-                    
-                    self?.locations = locations
+                    print(locationDetails)
                 }
             }
             catch {
@@ -114,18 +130,12 @@ class ViewModel: ObservableObject {
     }}
 
 struct LocationDetail: View {
-    
-    var location: [String: String] = [:]
-    
-    var text: String
-    var viewModel: ViewModel
-    
-    
+    var location: String
     
     var body: some View {
         VStack {
-            Text(text)
-                .navigationBarTitle(text.description.uppercased())
+            Text(location)
+                .navigationBarTitle(location.description.uppercased())
         }
     }
 }
@@ -138,7 +148,7 @@ struct ContentView: View {
             NavigationView {
                 List {
                     ForEach(viewModel.locations?.result ?? [""], id: \.self) { name in
-                        NavigationLink(name, destination: LocationDetail(text: name, viewModel: self.viewModel))
+                        NavigationLink(name, destination: LocationDetail(location: name))
                     }
                     .onDelete(perform: delete)
                 }
